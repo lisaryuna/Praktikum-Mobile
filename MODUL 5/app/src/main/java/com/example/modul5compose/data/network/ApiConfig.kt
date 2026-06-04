fcf641da542a6531319c6e6015cd1a0c767cc0f7
Lisa <fa037024@gmail.com>
2026-06-04 20:15:08 +0800

    feat: configure Retrofit client with KotlinX Serialization converter

--- MODUL 5/app/src/main/java/com/example/modul5compose/data/network/ApiConfig.kt
+++ MODUL 5/app/src/main/java/com/example/modul5compose/data/network/ApiConfig.kt
@@ -1,4 +1,26 @@
 package com.example.modul5compose.data.network
 
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+
 object ApiConfig {
+    private const val BASE_URL = "https://api.themoviedb.org/3/"
+
+    fun getApiService() : TmdbApiService {
+        val json = Json { ignoreUnknownKeys = true}
+        val contentType = "application/json".toMediaType()
+
+        val client = OkHttpClient.Builder().build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .build()
+
+        return retrofit.create(TmdbApiService::class.java)
+    }
 }

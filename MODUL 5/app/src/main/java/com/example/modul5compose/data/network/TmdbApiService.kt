b5c7121dad00f91c2426238f063be48b32faeba4
Lisa <fa037024@gmail.com>
2026-06-04 18:00:01 +0800

    feat: add Retrofit dependencies and setup TMDB API interface

--- MODUL 5/app/src/main/java/com/example/modul5compose/data/network/TmdbApiService.kt
+++ MODUL 5/app/src/main/java/com/example/modul5compose/data/network/TmdbApiService.kt
@@ -1,4 +1,15 @@
 package com.example.modul5compose.data.network
 
+import com.example.modul5compose.data.model.MovieResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface TmdbApiService {
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String = "en-US",
+        @Query("page") page: Int = 1,
+    ): MovieResponse
 }

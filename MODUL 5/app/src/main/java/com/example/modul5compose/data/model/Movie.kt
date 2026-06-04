c2bd864a8a5c947b334bc773740350efb034c220
Lisa <fa037024@gmail.com>
2026-06-04 16:53:49 +0800

    feat: create Movie data models and response mapping for TMDB API

--- MODUL 5/app/src/main/java/com/example/modul5compose/data/model/Movie.kt
+++ MODUL 5/app/src/main/java/com/example/modul5compose/data/model/Movie.kt
@@ -1,3 +1,24 @@
 package com.example.modul5compose.data.model
 
-data class Movie()
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Movie(
+    @SerialName("id")
+    val id: Int,
+
+    @SerialName("title")
+    val title: String,
+
+    @SerialName("overview")
+    val overview: String,
+
+    @SerialName("poster_path")
+    val posterPath: String?,
+
+    @SerialName("release_date")
+    val releaseDate: String?
+) {
+
+}

262d261a91c628475de1d882dae2a696279abd71
Lisa <fa037024@gmail.com>
2026-06-06 00:05:18 +0800

    feat: design MovieItemCard layout identically to Modul 4

--- MODUL 5/app/src/main/java/com/example/modul5compose/ui/screen/HomeScreen.kt
+++ MODUL 5/app/src/main/java/com/example/modul5compose/ui/screen/HomeScreen.kt
@@ -1,53 +1,41 @@
 package com.example.modul5compose.ui.screen
 
-import android.content.Intent
-import android.net.Uri
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.*
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.*
 import androidx.navigation.NavController
-import com.example.modul5compose.ui.theme.*
+import coil.compose.AsyncImage
+import com.example.modul5compose.MovieViewModel
 import com.example.modul5compose.R
-import com.example.modul5compose.ui.navigation.SongItemCard
-import com.example.modul5compose.SongViewModel
-import com.example.modul5compose.SongViewModelFactory
+import com.example.modul5compose.data.model.Movie
+import com.example.modul5compose.data.network.ApiConfig
+import com.example.modul5compose.data.repository.UiState
+import com.example.modul5compose.ui.theme.*
 
 @Composable
 fun HomeScreen(
     navController: NavController,
-    viewModel: SongViewModel = viewModel(factory = SongViewModelFactory("Koleksi Music Compose"))
+    viewModel: MovieViewModel
 ) {
-    val context = LocalContext.current
-    val songList by viewModel.songList.collectAsState()
+    val movieState by viewModel.movieState.collectAsState()
     val navEvent by viewModel.navigationEvent.collectAsState()
-    val intentEvent by viewModel.intentEvent.collectAsState()
 
     LaunchedEffect(navEvent) {
-        navEvent?.let { song ->
-            navController.navigate("detail/${song.id}")
+        navEvent?.let { movie ->
+            navController.navigate("detail/${movie.id}")
             viewModel.onNavigationHandled()
-        }
-    }
-
-    LaunchedEffect(intentEvent) {
-        intentEvent?.let { link ->
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
-            context.startActivity(intent)
-            viewModel.onIntentHandled()
         }
     }
 
@@ -80,29 +68,106 @@ fun HomeScreen(
             }
         }
 
-        LazyRow(contentPadding = PaddingValues(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-            items(songList) { song ->
-                SongItemCard(
-                    song = song,
-                    onDetailClick = { viewModel.onDetailClicked(song) },
-                    onLinkClick = { viewModel.onIntentClicked(song.externalLink) },
-                    Modifier.Companion.fillParentMaxWidth()
-                )
+        when (val state = movieState) {
+            is UiState.Loading -> {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    CircularProgressIndicator(color = Watermelon)
+                }
+            }
+            is UiState.Error -> {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Ups, Gagal: ${state.errorMessage}", color = Color.Red)
+                }
+            }
+            is UiState.Success -> {
+                val movieList = state.data
+
+                LazyRow(contentPadding = PaddingValues(horizontal = 16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+                    items(movieList) { movie ->
+                        MovieItemCard(
+                            movie = movie,
+                            onClick = { viewModel.onDetailClicked(movie)},
+                            Modifier.fillParentMaxWidth()
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                LazyColumn(contentPadding = PaddingValues(start = 16.dp, end = 16.dp, bottom = 16.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)) {
+                    items(movieList) { movie ->
+                        MovieItemCard(
+                            movie = movie,
+                            onClick = { viewModel.onDetailClicked(movie)},
+                            Modifier.fillMaxWidth()
+                        )
+                    }
+                }
             }
         }
+    }
+}
 
-        Spacer(modifier = Modifier.height(16.dp))
+@Composable
+fun MovieItemCard(movie: Movie, onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Card(
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(containerColor = PinkChampagne),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        modifier = modifier
+    ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp)
+        ) {
+            AsyncImage (
+                model = "${ApiConfig.IMAGE_BASE_URL}${movie.posterPath}",
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(110.dp)
+                    .clip(RoundedCornerShape(16.dp))
+            )
 
-        LazyColumn(contentPadding = PaddingValues(start = 16.dp, end = 16.dp, bottom = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)) {
-            items(songList) { song ->
-                SongItemCard(
-                    song = song,
-                    onDetailClick = { viewModel.onDetailClicked(song) },
-                    onLinkClick = { viewModel.onIntentClicked(song.externalLink) },
-                    Modifier.fillMaxWidth()
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Text(
+                    text = movie.title,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Text(
+                    text = "Rilis: ${movie.releaseDate}",
+                    color = Color.Black,
+                    fontSize = 14.sp
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row (
+                    horizontalArrangement = Arrangement.End,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Button(
+                        onClick =onClick,
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Watermelon,
+                            contentColor = Color.White
+                        ),
+                        contentPadding = PaddingValues(12.dp, 4.dp)
+                    ) {
+                        Text("Detail", fontSize = 12.sp)
+                    }
+                }
             }
         }
     }

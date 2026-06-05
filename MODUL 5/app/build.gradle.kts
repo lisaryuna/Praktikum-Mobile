97ca3b66bb8248be3bac8acab171f5455e82b751
Lisa <fa037024@gmail.com>
2026-06-06 00:23:20 +0800

    build: add Coil dependency for asynchronous image loading

--- MODUL 5/app/build.gradle.kts
+++ MODUL 5/app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.retrofit2:converter-kotlinx-serialization:3.0.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("io.coil-kt:coil-compose:2.6.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

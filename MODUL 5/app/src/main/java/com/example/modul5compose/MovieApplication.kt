c6dc9df8e3a730a44248a84964a44ac74509a2ff
Lisa <fa037024@gmail.com>
2026-06-06 10:53:29 +0800

    refactor: rename MusicApplication to MovieApplication and register in manifest

--- MODUL 5/app/src/main/java/com/example/modul5compose/MovieApplication.kt
+++ MODUL 5/app/src/main/java/com/example/modul5compose/MovieApplication.kt
@@ -3,7 +3,7 @@ package com.example.modul5compose
 import android.app.Application
 import timber.log.Timber
 
-class MusicApplication : Application() {
+class MovieApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())

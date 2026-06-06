4ba2afa692c94db8e8f5956823277d9dc87027b1
Lisa <fa037024@gmail.com>
2026-06-06 11:15:38 +0800

    fix: migrate to AppCompatActivity and update theme for app locale switching

--- MODUL 5/app/src/main/java/com/example/modul5compose/MainActivity.kt
+++ MODUL 5/app/src/main/java/com/example/modul5compose/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.modul5compose
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.*
 import com.example.modul5compose.ui.navigation.AppNavigation
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {

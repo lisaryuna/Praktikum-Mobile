e5651b2f73cd1b639f36d4e7ba45bf01d43353b9
Lisa <fa037024@gmail.com>
2026-06-06 11:55:59 +0800

    feat: integrate SharedPreferences in LanguageScreen to save user locale choice

--- MODUL 5/app/src/main/java/com/example/modul5compose/ui/screen/LanguageScreen.kt
+++ MODUL 5/app/src/main/java/com/example/modul5compose/ui/screen/LanguageScreen.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -16,9 +18,13 @@ import androidx.core.os.LocaleListCompat
 import androidx.navigation.NavController
 import com.example.modul5compose.ui.theme.*
 import com.example.modul5compose.R
+import com.example.modul5compose.data.local.UserPreference
 
 @Composable
 fun LanguageScreen(navController: NavController) {
+    val context = LocalContext.current
+    val userPreference = remember { UserPreference(context) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -35,14 +41,18 @@ fun LanguageScreen(navController: NavController) {
             modifier = Modifier.padding(bottom = 24.dp)
         )
         Button(
-            onClick = { setAppLocale("en") },
+            onClick = {
+                userPreference.saveLanguage("en")
+                setAppLocale("en") },
             colors = ButtonDefaults.buttonColors(containerColor = Watermelon, contentColor = Color.White),
             modifier = Modifier.fillMaxWidth()
         ) { Text(stringResource(R.string.btn_english))}
 
         Spacer(modifier = Modifier.height(16.dp))
         Button(
-            onClick = { setAppLocale("id") },
+            onClick = {
+                userPreference.saveLanguage("id")
+                setAppLocale("id") },
             colors = ButtonDefaults.buttonColors(containerColor = Watermelon, contentColor = Color.White),
             modifier = Modifier.fillMaxWidth()
         ) { Text(stringResource(R.string.btn_indonesia))}

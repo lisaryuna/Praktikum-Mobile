fc6f231030c4fd576dc7fc283c502d8d753cbac1
Lisa <fa037024@gmail.com>
2026-06-07 12:56:56 +0800

    feat(repository): implement offline-first caching logic with Room

--- MODUL 5/app/src/main/java/com/example/modul5compose/data/repository/MovieRepository.kt
+++ MODUL 5/app/src/main/java/com/example/modul5compose/data/repository/MovieRepository.kt
@@ -1,10 +1,12 @@
 package com.example.modul5compose.data.repository
 
 import com.example.modul5compose.BuildConfig
+import com.example.modul5compose.data.local.MovieDao
 import com.example.modul5compose.data.model.Movie
 import com.example.modul5compose.data.network.ApiConfig
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import timber.log.Timber
 
 sealed class UiState<out T : Any?> {
     object Loading : UiState<Nothing>()
@@ -12,18 +14,35 @@ sealed class UiState<out T : Any?> {
     data class Error(val errorMessage: String) : UiState<Nothing>()
 }
 
-class MovieRepository {
+class MovieRepository(private val movieDao: MovieDao) {
     private val apiService = ApiConfig.getApiService()
 
     private val apiKey = BuildConfig.TMDB_API_KEY
 
     fun getPopularMovies(language: String) : Flow<UiState<List<Movie>>> = flow {
         emit(UiState.Loading)
+
         try {
             val response = apiService.getPopularMovies(apiKey = apiKey, language = language)
-            emit(UiState.Success(response.results))
+            val moviesFromApi = response.results
+
+            movieDao.deleteAllMovies()
+            movieDao.insertMovies(moviesFromApi)
+            Timber.d("Data berhasil diambil dari API dan disimpan ke database lokal.")
         } catch (e: Exception) {
-            emit(UiState.Error(e.message ?: "Terjadi kesalahan jaringan"))
+            Timber.e("Gagal mengambil data dari API, mencoba menggunakan data lokal. Error: ${e.message}")
+        }
+
+        try {
+            movieDao.getAllMovies().collect { localMovies ->
+                if (localMovies.isNotEmpty()) {
+                    emit(UiState.Success(localMovies))
+                } else {
+                    emit(UiState.Error("Tidak ada koneksi internet dan belum ada data yang tersimpan."))
+                }
+            }
+        } catch (e: Exception) {
+            emit(UiState.Error("Gagal membaca database: ${e.message}"))
         }
     }
 }
